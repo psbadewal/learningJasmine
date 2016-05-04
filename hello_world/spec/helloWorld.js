72d5b28dcd0ee2f092124901e9ea57d1f23c1903
psbadewal <psbadewal@icloud.com>
2016-05-04 23:34:01 +0100

    Requring request and base_url

--- hello_world/spec/helloWorld.js
+++ hello_world/spec/helloWorld.js
@@ -1,7 +1,13 @@
+var request = require("request");
+
+var base_url = "http://localhost:3000/"
+
 describe("Hello World Server", function() {
   describe("GET /", function(){
     it("returns status code 200", function(){
-      
-    })
-  })
-})
+      request.get(base_url, function(error, response, body) {
+
+      });
+    });
+  });
+});
